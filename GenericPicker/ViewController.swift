b1327e557f6b2a5aa612a3ef9450b4c731ee6f32
adar tzeiri <adar-tz@yit.co.il>
2020-07-22 12:28:09 +0300

    Initial Commit

---
+++ GenericPicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GenericPicker
+//
+//  Created by Adar Tzeiri on 22/07/2020.
+//  Copyright © 2020 Adar Tzeiri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
